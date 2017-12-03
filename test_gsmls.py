4e9887cb980850ec55cce9be5e99c78c3b8aacc9
Al Johri <al.johri@gmail.com>
2017-12-03 08:21:43 -0500

    reformat test_gsmls

--- test_gsmls.py
+++ test_gsmls.py
@@ -4,7 +4,11 @@ import html2text
 from gsmls import get_listings, get_listing_detail_preview
 
 def test_gsmls():
-    for listing in get_listings('Hudson', min_list_price='350000', max_list_price='500000', min_bedrooms='3', min_bathrooms='2'):
+    for listing in get_listings('Hudson',
+                                min_list_price='350000',
+                                max_list_price='500000',
+                                min_bedrooms='3',
+                                min_bathrooms='2'):
         print(json.dumps(listing))
     last_listing_id = listing['id']
     html = get_listing_detail_preview(last_listing_id)
